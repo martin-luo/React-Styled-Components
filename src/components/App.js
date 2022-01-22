90b4d7070edbbac5b1d40f1db743b5fbd4762e0f
Martin Luo <aluo1.developer@gmail.com>
2022-01-22 17:32:10 +1100

    first styled component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
+import { Button } from "./common/";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>App</h1>
       </header>
+
+      <Button>Test</Button>
     </div>
   );
 }
