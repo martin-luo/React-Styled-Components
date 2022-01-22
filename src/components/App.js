08f2bb4774f8f6c54209b1258761c4741416d86b
Martin Luo <aluo1.developer@gmail.com>
2022-01-22 17:40:34 +1100

    add global styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
+import { createGlobalStyle } from "styled-components";
 import { Button } from "./common/";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: "Kaushan Script";
+  }
+`;
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <header className="App-header">
         <h1>App</h1>
       </header>
 
-      <Button>Test</Button>
+      <Button>Primary button</Button>
+      <Button secondary>Secondary button</Button>
     </div>
   );
 }
