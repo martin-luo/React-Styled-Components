63643af5c3a2602a3b0a90ac4b8ac105416c272e
Martin Luo <aluo1.developer@gmail.com>
2022-01-23 16:44:00 +1100

    use keyframes to make animation

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -3,3 +3,4 @@ export * from "./Header";
 export * from "./Input";
 export * from "./PageLayout";
 export * from "./PasswordInput";
+export * from "./Spinner";
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Input, PageLayout, PasswordInput } from "../common";
+import { Button, Input, PageLayout, PasswordInput, Spinner } from "../common";
 
 const Form = styled.form`
   width: 100%;
@@ -10,10 +10,18 @@ const Form = styled.form`
   padding: 16px;
   box-sizing: border-box;
   color: black;
+
+  .alt-text {
+    text-align: center;
+    margin: 10px 0;
+  }
 `;
+
+let timeout;
 
 export const Login = () => {
   const [formField, setFormField] = useState({ username: "", password: "" });
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleInputChange = (e) => {
     e.persist();
@@ -24,22 +32,55 @@ export const Login = () => {
     }));
   };
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, []);
+
   return (
     <PageLayout>
       <h1>Login</h1>
-      <Form>
-        <Input
-          name="username"
-          placeholder="Username"
-          value={formField.username}
-          type="text"
-          onChange={handleInputChange}
-        />
-        <PasswordInput
-          name="password"
-          value={formField.password}
-          onChange={handleInputChange}
-        />
+      <Form onSubmit={handleFormSubmit}>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <Input
+              name="username"
+              placeholder="Username"
+              value={formField.username}
+              type="text"
+              onChange={handleInputChange}
+            />
+            <PasswordInput
+              name="password"
+              value={formField.password}
+              onChange={handleInputChange}
+            />
+            <Button large type="submit" disabled={isLoading}>
+              {isLoading ? "Loading" : "Login"}
+            </Button>
+            {!isLoading && (
+              <>
+                <div className="alt-text">or</div>
+                <Button secondary type="button">
+                  Register
+                </Button>
+              </>
+            )}
+          </>
+        )}
       </Form>
     </PageLayout>
   );
